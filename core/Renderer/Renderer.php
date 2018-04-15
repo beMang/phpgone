76e5797a286c2bd120d439c1a5b91c5cf847b191
beMang <antonuttiadrien@gmail.com>
2018-04-15 15:42:44 +0200

    Twig renderer

--- core/Renderer/Renderer.php
+++ core/Renderer/Renderer.php
@@ -7,7 +7,6 @@ use phpGone\Core\ApplicationComponent;
 class Renderer{
 
     public static function render($view, $datas){
-        $fileToRender = __DIR__ . '/../../app/views/' . $view;
         if (!file_exists($fileToRender)) {
             throw new \RuntimeException('La vue spécifiée n\'existe pas' . $fileToRender);
         }
@@ -18,6 +17,36 @@ class Renderer{
     }
 
     public static function twigRender($view, $datas){
-        //TO_DO
+        $loaderTwig = new \Twig_Loader_Filesystem(__DIR__ . '/../../app/views/');
+        $twig = new \Twig_Environment($loaderTwig, [
+            'cache' => false
+        ]);
+        /*
+        Extension
+        foreach ($this->getApp()->getConfig()->get('TwigExtensions') as $extension) {
+            $twig->addExtension(new $extension);
+        }
+        */
+        echo $twig->render($view, $datas);
     }
+
+    /*
+    public static function twigRenderWithCache(){
+        if (is_null($directory)) {
+            $dir = __DIR__ . '../../../../../../tmp/cache/twig/';
+        } else {
+            $dir = $directory;
+        }
+        $loaderTwig = new \Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT'] . '/app/views/');
+        $twig = new \Twig_Environment($loaderTwig, [
+            'cache' => $dir
+        ]);
+        //Ajout des extensions
+        foreach ($this->getApp()->getCongig()->get('TwigExtensions') as $extension) {
+            $twig->addExtension(new $extension);
+        }
+        return $twig->render($this->contentFile, $this->vars);
+    }
+    */
+
 }
