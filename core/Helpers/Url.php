c4c7eafa64fbeaa187f8047039a5d2ad0872cc1e
beMang <antonuttiadrien@gmail.com>
2018-05-01 12:05:25 +0200

    Url extensions support custom url

--- core/Helpers/Url.php
+++ core/Helpers/Url.php
@@ -5,27 +5,27 @@ namespace phpGone\Helpers;
 use phpGone\Helpers\Helper;
 
 class Url extends Helper{
-    public function getTmpPath(){
-        return __DIR__ . '/../../tmp/';
+    public function getTmpPath($custom = ''){
+        return __DIR__ . '/../../tmp/' . $custom;
     }
 
-    public function getAppPath(){
-        return __DIR__ . '/../../app/';
+    public function getAppPath($custom = ''){
+        return __DIR__ . '/../../app/' . $custom;
     }
 
-    public function getTestsPath(){
-        return __DIR__ . '/../../tests/';
+    public function getTestsPath($custom = ''){
+        return __DIR__ . '/../../tests/' . $custom;
     }
 
-    public function getAssetsPath(){
-        return $this->getAppPath() . 'assets/';
+    public function getAssetsPath($custom = ''){
+        return $this->getAppPath('assets/');
     }
 
-    public function getRelativeAssetsPath(){
-        return $this->getRelativeAppPath() . 'assets/';
+    public function getRelativeAssetsPath($custom = ''){
+        return $this->getRelativeAppPath('assets/');
     }
 
-    public function getRelativeAppPath(){
-        return $this->getConfig()->get('basePath') . 'app/';
+    public function getRelativeAppPath($custom = ''){
+        return $this->getConfig()->get('basePath') . 'app/' . $custom;
     }
 }
