c0e6db0c9a91d75ffc242eafa44d78f66d16c2ae
beMang <antonuttiadrien@gmail.com>
2018-05-05 18:03:29 +0200

    reset index.php

--- index.php
+++ index.php
@@ -5,8 +5,4 @@ require __DIR__ . "/vendor/autoload.php";
 $app = new phpGone\Core\Application(__DIR__ . '/app/config.php', \GuzzleHttp\Psr7\ServerRequest::fromGlobals());
 $app->addMiddlewares(\phpGone\Middlewares\TrailingSlashMiddleware::class);
 $response = $app->run();
-
-$manager = \phpGone\Database\DBManager::getInstance($app);
-$manager->addDatabase('base');
-$query = new \phpGone\Database\Query('user_test', 'base');
-var_dump($query->getAll());
+$app->send();
