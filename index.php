d51e6a4f2dcda26416d6deeb717cbb192dc385cd
beMang <antonuttiadrien@gmail.com>
2018-05-27 14:12:12 +0200

    Adding dependencies

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ session_start();
 require __DIR__ . "/vendor/autoload.php";
 
 $app = new phpGone\Core\Application(__DIR__ . '/app/config.php', \GuzzleHttp\Psr7\ServerRequest::fromGlobals());
-$app->addMiddlewares(\phpGone\Middlewares\TrailingSlashMiddleware::class);
+$app->addMiddleware(\phpGone\Middlewares\TrailingSlashMiddleware::class);
 $response = $app->run();
 $app->send();
